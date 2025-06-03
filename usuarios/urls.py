404af9189f442f5b06076146c13e7488f8c61401
JunixOs <yonelsilva75@gmail.com>
2025-06-03 15:09:40 -0500

    Añade paginas de login y register desde el index.

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.Login, name='Login Users'),
-    path('register/', views.Register, name='Register Users'),
+    path('login/', views.Login, name='login'),
+    path('register/', views.Register, name='register'),
 ]
